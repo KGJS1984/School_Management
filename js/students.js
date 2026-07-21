4c4d118cf9cb369cfda5e97c5166fb86b05a87ac
KGJS1984 <usmanabrarhadi2026@gmail.com>
2026-07-21 20:03:56 +0600

    Update students.js

--- js/students.js
+++ js/students.js
@@ -1,1 +1,65 @@
+function loadStudents() {
 
+    let students = JSON.parse(localStorage.getItem("students")) || [];
+    let search = document.getElementById("search").value.toLowerCase();
+
+    let table = document.getElementById("studentTable");
+    table.innerHTML = "";
+
+    students.forEach((student, index) => {
+
+        if (
+            student.name.toLowerCase().includes(search) ||
+            student.class.toLowerCase().includes(search)
+        ) {
+
+            table.innerHTML += `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${student.name}</td>
+                <td>${student.father}</td>
+                <td>${student.class}</td>
+                <td>${student.roll}</td>
+                <td>${student.mobile}</td>
+
+                <td>
+                    <button class="btn btn-warning btn-sm"
+                        onclick="editStudent(${index})">
+                        Edit
+                    </button>
+
+                    <button class="btn btn-danger btn-sm"
+                        onclick="deleteStudent(${index})">
+                        Delete
+                    </button>
+                </td>
+            </tr>
+            `;
+        }
+
+    });
+
+}
+
+function deleteStudent(index) {
+
+    if (confirm("Delete this student?")) {
+
+        let students = JSON.parse(localStorage.getItem("students")) || [];
+
+        students.splice(index, 1);
+
+        localStorage.setItem("students", JSON.stringify(students));
+
+        loadStudents();
+    }
+
+}
+
+function editStudent(index) {
+
+    alert("Edit Module v2.0 আসছে...");
+
+}
+
+window.onload = loadStudents;
