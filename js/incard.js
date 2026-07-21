8e615fe8c22609a7d3bda3a94d1bf6fedbfe43d9
KGJS1984 <usmanabrarhadi2026@gmail.com>
2026-07-21 22:04:23 +0600

    Update incard.js

--- js/incard.js
+++ js/incard.js
@@ -1,1 +1,45 @@
+// Student ID Card Generator
 
+function generateIDCard(student) {
+
+    let card = `
+    <div class="id-card">
+        <div class="school-name">
+            Gobindaganj KG & Junior School
+        </div>
+
+        <img src="${student.photo}" 
+        class="student-photo">
+
+        <h3>${student.name}</h3>
+
+        <p><b>Class:</b> ${student.class}</p>
+        <p><b>Roll:</b> ${student.roll}</p>
+        <p><b>ID:</b> ${student.id}</p>
+        <p><b>Mobile:</b> ${student.mobile}</p>
+
+        <div class="signature">
+            Principal Signature
+        </div>
+    </div>
+    `;
+
+    document.getElementById("cardBox").innerHTML = card;
+}
+
+
+// Example Student Data
+
+let student = {
+
+    name:"Rahim Ahmed",
+    class:"Five",
+    roll:"12",
+    id:"2026001",
+    mobile:"017XXXXXXXX",
+    photo:"student.jpg"
+
+};
+
+
+generateIDCard(student);
