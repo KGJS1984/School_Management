7637740109ce90adcab2c5995aab88e70eaa791a
KGJS1984 <usmanabrarhadi2026@gmail.com>
2026-07-21 14:04:31 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-alert("Line 1");
-
 function login() {
-    alert("Login OK");
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    if (email === "admin@gmail.com" && password === "123456") {
+        window.location.assign("dashboard.html");
+    } else {
+        alert("Wrong Email or Password");
+    }
 }
