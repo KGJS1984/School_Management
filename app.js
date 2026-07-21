5029d6f372877ed68b613c126001e60ce810c299
KGJS1984 <usmanabrarhadi2026@gmail.com>
2026-07-21 13:54:52 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-alert("app.js is loaded");
+alert("Line 1");
 
 function login() {
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-
-    if (email === "admin@gmail.com" && password === "123456") {
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Wrong Email or Password");
-    }
-}
-function login() {
-    alert("Login button works!");
-
-    let email = document.getElementById("email").value.trim();
-    let password = document.getElementById("password").value.trim();
-
-    if (email === "admin@gmail.com" && password === "123456") {
-        alert("Success");
-        window.location.href = "dashboard.html";
-    } else {
-        alert("Wrong Email or Password");
-    }
+    alert("Login OK");
 }
