d1602a8e2f2f9d2f6effb0d830ec4c0a09588e9b
KGJS1984 <usmanabrarhadi2026@gmail.com>
2026-07-23 00:52:50 +0600

    Update attendance_report.js

--- js/attendance_report.js
+++ js/attendance_report.js
@@ -1,1 +1,59 @@
+// =========================
+// Attendance Report
+// =========================
 
+function loadAttendanceReport() {
+
+    let date = document.getElementById("reportDate").value;
+
+    if (date === "") {
+        alert("Please select a date.");
+        return;
+    }
+
+    let report =
+        JSON.parse(localStorage.getItem("attendance_" + date)) || [];
+
+    let table = document.getElementById("reportTable");
+
+    table.innerHTML = "";
+
+    let present = 0;
+    let absent = 0;
+    let leave = 0;
+
+    report.forEach((student, index) => {
+
+        if(student.status === "Present") present++;
+        if(student.status === "Absent") absent++;
+        if(student.status === "Leave") leave++;
+
+        table.innerHTML += `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${student.name}</td>
+            <td>${student.class}</td>
+            <td>${student.roll}</td>
+            <td>${student.status}</td>
+        </tr>
+        `;
+
+    });
+
+    table.innerHTML += `
+    <tr class="table-primary">
+        <td colspan="5">
+            <b>
+            Present: ${present}
+            &nbsp;&nbsp;|
+            &nbsp;&nbsp;
+            Absent: ${absent}
+            &nbsp;&nbsp;|
+            &nbsp;&nbsp;
+            Leave: ${leave}
+            </b>
+        </td>
+    </tr>
+    `;
+
+}
