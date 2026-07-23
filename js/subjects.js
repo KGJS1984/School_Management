0e788d121311fdf4940c6160595d595658381416
KGJS1984 <usmanabrarhadi2026@gmail.com>
2026-07-23 16:19:43 +0600

    Update subjects.js

--- js/subjects.js
+++ js/subjects.js
@@ -1,1 +1,46 @@
+// =========================
+// Subject Management
+// =========================
 
+function saveSubject() {
+
+    let subject = {
+        id: Date.now(),
+        name: document.getElementById("subjectName").value,
+        code: document.getElementById("subjectCode").value,
+        class: document.getElementById("subjectClass").value,
+        teacher: document.getElementById("subjectTeacher").value
+    };
+
+    let subjects =
+    JSON.parse(localStorage.getItem("subjects")) || [];
+
+    let editIndex =
+    localStorage.getItem("subjectEditIndex");
+
+    if(editIndex !== null){
+
+        subjects[editIndex] = subject;
+
+        localStorage.removeItem("subjectEditIndex");
+
+        alert("✅ Subject Updated Successfully");
+
+    }else{
+
+        subjects.push(subject);
+
+        alert("✅ Subject Saved Successfully");
+
+    }
+
+    localStorage.setItem(
+        "subjects",
+        JSON.stringify(subjects)
+    );
+
+    document.querySelector("form").reset();
+
+    window.location.href = "subjects_list.html";
+
+}
